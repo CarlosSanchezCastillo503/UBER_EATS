f352a6dc680b4a5c505baab52326686fbe2915db
carlosneos503 <63827845+carlosneos503@users.noreply.github.com>
2020-10-26 17:56:54 -0600

    proceso1

---
+++ UBER_EATS/UBER_EATS/Paginacion/Pagina Principal/index.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UBER_EATS.Paginacion.Pagina_Principal
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
